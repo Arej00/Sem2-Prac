555be67714598fe1bafc6e75058be0e3c5c77534
Arej00 <a1904627@adelaide.edu.au>
2024-03-14 01:23:20 +1030

    Files

---
+++ main-1-2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+extern int is_identity(int array[10][10]);
+using namespace std;
+
+
+int main(){
+    int array[10][10]={0};
+    for(int i=0;i<10;i++){
+        for(int j=0;j<10;j++){
+            if(i==j){
+                array[i][j]=1;
+            }
+        }       
+
+    }
+    int result=is_identity(array);
+    cout<<result;
+    return 0;
+
+ }
